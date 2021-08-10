3265d4c37e12d5529abbd6749f011916b5ab9538
fmagoge <fmagoge@gmail.com>
2021-08-10 17:16:24 +0200

    Realtime Chat Implementation
    will implement a real-time chat facility that will cover 2 main features,
    1. Send a message
    2. Listen for incoming messages.

--- app/src/main/java/com/dmatrix/chat/models/ChatMessage.java
+++ app/src/main/java/com/dmatrix/chat/models/ChatMessage.java
@@ -1,5 +1,9 @@
 package com.dmatrix.chat.models;
 
+
+import java.util.Date;
+
 public class ChatMessage {
     public String senderId, receiverId, message, dateTime;
+    public Date dateObject;
 }
